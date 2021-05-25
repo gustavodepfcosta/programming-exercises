499e2ac2e893d98636dc20a893f2163cc0a17d46
Gustavo Costa <gustavodepfcosta@outlook.com>
2021-05-25 13:51:54 -0300

    Reapproaching the exercise with more caution and trying to code with a better time complexity

--- max_subarray_non_negative.py
+++ max_subarray_non_negative.py
@@ -12,31 +12,67 @@ NOTE:
 
 If there is a tie, then compare with segment's length and return segment which has maximum length.
 If there is still a tie, then return the segment with minimum starting index.
+
+I'm gonna need the following variables that will hold the value according to its given description
+
+NOTE: I WILL CHECK THE END OF A SUB-ARRAY, NOT THE BEGINNING
+
+***** I will access: *****
+- CURRENT_ELEMET_VALUE ----> It will hold the value of the element currently being analyzed on that iteration - I will check wheter it is a positive or a negative and do the max accordingly;
+
+- CURRENT_ELEMENT_INDEX ----> It will hold the index value of the element currently being analyzed on that iteration. Positive indexes.
+***** I will hold: *****
+
+CURRENT_SUM ----> This variable will hold the current sum of the sequential positive array values in that iteration; NOTE: THIS VARIABLE WILL BE ASSIGNED AS SOON AS I ANALYZE WHETER THE NUMBER IS POSITIVE OR NOT AND IF IT'S SEQUENTIAL.
+
+LAST_SUM ----> This variable will be equal to zero until I find a negative number. Given the possibility of encountering several different sequences of positive numbers, I need to store the biggest sum as I'm encountering them so I can compare then each time a find a negative number; NOTE> THIS VARIABLE WILL BE ASSIGNED AS SOON AS I ENCOUNTER A NEGATIVE NUMBER, THAT IS BECAUSE I NEED TO HOLD THE CURRENT_SUM VARIABLE FOR FURTHER COMPARISON AND THUS I WILL ZERO THE CURRENT_SUM VARIABLE SO I CAN RESTART SUMMING.
+
+===== So far, I can only calculate the maximum sum sub-array of non negatives in an array that has no ties. Plus, I can't tell the indexes of those elements making up the biggest sum; So in order to get rid of a tie, I need to not only store the elements indexes and their respective values, but I have to compare the length of each segment which will only be possible if I'm aware of the number of indexes that make up that sub-array =====
+
+**** I will hold a list containing a dictionary describing each positive sub-array *****
+
+SUB_ARRAY_LENGTH_COUNTER ----> In order to compare various sub-arrays, I need to set a counter counting the sub-array elements so I can use it in the METADATA variable
+
+METADATA ----> The list will be made of using the following standard
+
+
 """
 
-A = [1, 2, 5, -7, 2, 3]
+A = [1, 11, -3, -7, 2, 3, 5]
 
+def sub_array_parser(array):
+    current_sum = 0
+    biggest_sum = 0
+    current_sub_array_length = 0
+    biggest_sub_array_length = 0
+    metadata = []
 
-def max_sum_positive_sub_array(array):
-    updated_sum = 0
-    last_counter_holder = 0
-    element_last_index = 0
+    for index, value in enumerate(array):
+        if value >= 0:
+            current_sum += value
 
-    for updated_element_index, element_value in enumerate(array):
-        print(f'updated_element_index: {updated_element_index} |||| element index {element_value}')
-        if element_value >= 0:
-            if updated_element_index - element_last_index == 1:
-                updated_sum += element_value
-                last_counter_holder = updated_sum
+            if current_sum == value:
+                current_sub_array_starting_index = index
+
+        if value < 0:
+            sub_array_length = (index - 1) - current_sub_array_starting_index
+
+            if current_sum > biggest_sum:
+                biggest_sum = current_sum
             
-            else:
-                print(f'updated sum {updated_sum} |||| last counter holder : {last_counter_holder}')
+            if current_sum == biggest_sum:
+                if current_sub_array_length > biggest_sub_array_length:
+                    biggest_sub_array_length = current_sub_array_length
 
-                print(last_counter_holder)
-        
-        element_last_index = updated_element_index
+                    return [biggest_sum, current_sub_array_starting_index, biggest_sub_array_length]
 
-    # print(last_counter_holder)   
+                if current_sub_array_length == biggest_sub_array_length:
+                    if ():
+                        pass
 
+            current_sum = 0
 
-max_sum_positive_sub_array(A)
+    
+    print(current_sum, biggest_sum)
+
+sub_array_parser(A)
